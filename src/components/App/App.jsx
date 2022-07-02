88fe33889791ceef50cd71b88bf002c97098463c
Marta Żak <martysik19@gmail.com>
2022-07-02 21:47:41 +0200

    added option to save contacts in local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,12 +7,7 @@ import s from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
 
     filter: '',
   };
@@ -48,6 +43,22 @@ export class App extends Component {
 
     this.setState({ contacts: clearedContactList });
   };
+
+  componentDidUpdate(prevState) {
+    const { contacts } = this.state;
+
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (savedContacts.length > 0) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
